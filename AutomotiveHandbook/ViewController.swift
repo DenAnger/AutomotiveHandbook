034383588bc1e3ce311ac2990dc9b486ca680a0f
Denis Abramov <denmiracle@mail.ru>
2020-09-17 14:21:50 +0700

    first commit

---
+++ AutomotiveHandbook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutomotiveHandbook
+//
+//  Created by Denis Abramov on 17.09.2020.
+//  Copyright © 2020 Denis Abramov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
