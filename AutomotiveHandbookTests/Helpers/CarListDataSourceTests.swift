b2f1eb5fc3c3b15ada95fbb00b2f58f452355676
Denis Abramov <denmiracle@mail.ru>
2020-09-18 12:31:02 +0700

    refactoring

--- AutomotiveHandbookTests/Helpers/CarListDataSourceTests.swift
+++ AutomotiveHandbookTests/Helpers/CarListDataSourceTests.swift
@@ -12,69 +12,61 @@ import XCTest
 class CarListDataSourceTests: XCTestCase {
 
     var dataSource: CarListDataSource!
-    var tableView: UITableView!
+    var mockTableView: MockTableView!
     var carListViewController: CarListViewController!
+    var car: Car!
     
     override func setUpWithError() throws {
         dataSource = CarListDataSource()
         dataSource.carManager = CarManager()
+        
+        mockTableView = MockTableView.mockTableView(withDataSource: dataSource)
+        car = Car(yearOfIssue: "Foo",
+                  manufacture: "Bar",
+                  model: "Baz",
+                  bodyType: "Bat")
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         carListViewController = storyboard.instantiateViewController(
             identifier: "CarListViewController"
             ) as? CarListViewController
         carListViewController.loadViewIfNeeded()
-        
-        tableView = carListViewController.tableView
-        tableView.dataSource = dataSource
     }
 
     override func tearDownWithError() throws {
         dataSource = nil
-        tableView = nil
+        mockTableView = nil
         carListViewController = nil
+        car = nil
     }
     
     func testHasOneSection() {
-        let numberOfSection = tableView.numberOfSections
+        let numberOfSection = mockTableView.numberOfSections
         XCTAssertEqual(numberOfSection, 1)
     }
     
     func testNumberOfRowEqualsCarListCount() {
-        dataSource.carManager?.add(car: Car(yearOfIssue: "Foo",
-                                            manufacture: "Bar",
-                                            model: "Baz",
-                                            bodyType: "Bat"))
-        XCTAssertEqual(tableView.numberOfRows(inSection: 0), 1)
+        dataSource.carManager?.add(car: car)
+        XCTAssertEqual(mockTableView.numberOfRows(inSection: 0), 1)
         
         dataSource.carManager?.add(car: Car(yearOfIssue: "Bat",
                                             manufacture: "Baz",
                                             model: "Bar",
                                             bodyType: "Foo"))
-        tableView.reloadData()
-        XCTAssertEqual(tableView.numberOfRows(inSection: 0), 2)
+        mockTableView.reloadData()
+        XCTAssertEqual(mockTableView.numberOfRows(inSection: 0), 2)
     }
     
     func testCellForRowAtIndexPathReturnsCarCell() {
-        dataSource.carManager?.add(car: Car(yearOfIssue: "Foo",
-                                            manufacture: "Bar",
-                                            model: "Baz",
-                                            bodyType: "Bat"))
-        tableView.reloadData()
+        dataSource.carManager?.add(car: car)
+        mockTableView.reloadData()
         
-        let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0))
+        let cell = mockTableView.cellForRow(at: IndexPath(row: 0, section: 0))
         XCTAssertTrue(cell is CarCell)
     }
     
     func testCellForRowDequeuesCellFromTableView() {
-        let mockTableView = MockTableView()
-        mockTableView.dataSource = dataSource
-        mockTableView.register(CarCell.self, forCellReuseIdentifier: "cell")
-        
-        dataSource.carManager?.add(car: Car(yearOfIssue: "Foo",
-                                            manufacture: "Bar",
-                                            model: "Baz",
-                                            bodyType: "Bat"))
+        dataSource.carManager?.add(car: car)
         mockTableView.reloadData()
         
         _ = mockTableView.cellForRow(at: IndexPath(row: 0, section: 0))
@@ -82,14 +74,6 @@ class CarListDataSourceTests: XCTestCase {
     }
     
     func testCellForRowCallsConfigureCell() {
-        let mockTableView = MockTableView()
-        mockTableView.dataSource = dataSource
-        mockTableView.register(MockCarCell.self, forCellReuseIdentifier: "cell")
-        
-        let car = Car(yearOfIssue: "Foo",
-                      manufacture: "Bar",
-                      model: "Baz",
-                      bodyType: "Bat")
         dataSource.carManager?.add(car: car)
         mockTableView.reloadData()
         
@@ -111,6 +95,16 @@ extension CarListDataSourceTests {
             cellIsDequeued = true
             return super.dequeueReusableCell(withIdentifier: identifier,
                                              for: indexPath)
+        }
+        
+        static func mockTableView(
+            withDataSource dataSource: UITableViewDataSource
+        ) -> MockTableView {
+            let mockTableView = MockTableView()
+            mockTableView.dataSource = dataSource
+            mockTableView.register(MockCarCell.self,
+                                   forCellReuseIdentifier: "cell")
+            return mockTableView
         }
     }
     
